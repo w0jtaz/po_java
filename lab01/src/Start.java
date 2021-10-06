b7bada893b4f6364eba84af2b98c6652d09404eb
w0jtaz <wojtaz@vp.pl>
2021-10-06 21:21:29 +0200

    lab01

---
+++ lab01/src/Start.java
@@ -0,0 +1,26 @@
+public class Start {
+    public static void main(String[] args){
+
+        //zadanie1
+        System.out.println(30+31+30);
+
+        //zadanie2
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+
+        //zadanie3
+        System.out.println(1*2*3*4*5*6*7*8*9*10);
+
+        //zadanie4
+        System.out.println(1000+(1000*0.06/12)*3);
+
+        //zadanie6
+        System.out.println(" "+"/"+"/"+"/"+"/"+"/");
+        System.out.println("+"+'"'+'"'+'"'+'"'+'"'+"+");
+        System.out.println("("+"|"+"o"+" "+"o"+"|"+")");
+        System.out.println(" "+"|"+" "+"^"+" "+"|");
+        System.out.println("|"+"‘"+"_"+"’"+"|");
+        String printable = "Znaki specjalne \uFFE6";
+        System.out.println(printable);
+
+    }
+}
