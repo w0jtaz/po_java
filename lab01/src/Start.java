f7f6414ffbdd18167c6170314268879749ca25f2
w0jtaz <wojtaz@vp.pl>
2021-10-12 12:57:18 +0200

    lab01

--- lab01/src/Start.java
+++ lab01/src/Start.java
@@ -13,6 +13,11 @@ public class Start {
         //zadanie4
         System.out.println(1000+(1000*0.06/12)*3);
 
+        //zadanie5
+        System.out.println("+------+");
+        System.out.println("| Java |");
+        System.out.println("+------+");
+
         //zadanie6
         System.out.println(" "+"/"+"/"+"/"+"/"+"/");
         System.out.println("+"+'"'+'"'+'"'+'"'+'"'+"+");
@@ -21,6 +26,21 @@ public class Start {
         System.out.println("|"+"‘"+"_"+"’"+"|");
         String printable = "Znaki specjalne \uFFE6";
         System.out.println(printable);
+
+        //zadanie10
+        System.out.println("Miasto 44\nChłopaki nie płaczą\nPitbul niebezpieczne kobiety");
+
+        //zadanie11
+        System.out.println("\nWisława Szymborska, Dłoń");
+        System.out.println("Dwadzieścia siedem kości,\n" +
+                "trzydzieści pięć mięśni,\n" +
+                "około dwóch tysięcy komórek nerwowych\n" +
+                "w każdej opuszce naszych pięciu palców.\n" +
+                "To zupełnie wystarczy,\n" +
+                "żeby napisać „Mein Kampf”\n" +
+                "albo „Chatkę Puchatka”.\n" +
+                "\n" +
+                "(Wystarczy, 2011)");
 
     }
 }
