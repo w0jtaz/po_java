3d180d9b81454404f139f0817fbe357f7764d851
w0jtaz <wojtaz@vp.pl>
2021-10-26 19:57:01 +0200

    update

--- lab3/src/Start.java
+++ lab3/src/Start.java
@@ -45,7 +45,7 @@ public class Start {
         System.out.println("Suma liczb dodatnich: " + sumaDodatnich(tablica));
         System.out.println("Suma liczb ujemnych: " + sumaUjemnych(tablica));
         System.out.println("Długość maksymalna ciągu dodatnich: " + dlugoscMaksymalnegoCiaguDodatnich(tablica));
-        odwrocFragment(tablica, 1, 5);
+        odwrocFragment(tablica, 2,3 );
         wypisz(tablica);
         signum(tablica);
         wypisz(tablica);
@@ -72,59 +72,59 @@ public class Start {
         return licznik;
     }
 
-    public static int ileDodatnich(int tab[]){
+    public static int ileDodatnich(int tab[]) {
         int licznik = 0;
-        for(int i = 0; i < tab.length;i++){
-            if(tab[i] > 0){
-                licznik ++;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] > 0) {
+                licznik++;
             }
         }
         return licznik;
     }
 
-    public static int ileUjemnych(int tab[]){
+    public static int ileUjemnych(int tab[]) {
         int licznik = 0;
-        for(int i = 0; i < tab.length;i++){
-            if(tab[i] < 0){
-                licznik ++;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] < 0) {
+                licznik++;
             }
         }
         return licznik;
     }
 
-    public static int ileZerowych(int tab[]){
+    public static int ileZerowych(int tab[]) {
         int licznik = 0;
-        for(int i = 0; i < tab.length;i++){
-            if(tab[i] == 0){
-                licznik ++;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] == 0) {
+                licznik++;
             }
         }
         return licznik;
     }
 
-    public static int Maksymalne(int tab[]){
+    public static int Maksymalne(int tab[]) {
         int max = tab[0];
-        for(int i = 0; i < tab.length;i++){
-            if(tab[i] > max){
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] > max) {
                 max = tab[i];
             }
         }
         return max;
     }
 
-    public static int ileMaksymalnych(int tab[]){
+    public static int ileMaksymalnych(int tab[]) {
         int licznik = 0;
-        for(int i = 0; i < tab.length;i++){
-            if(tab[i] == Maksymalne(tab)){
-                licznik ++;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] == Maksymalne(tab)) {
+                licznik++;
             }
         }
         return licznik;
     }
 
-    public static int sumaDodatnich(int tab[]){
+    public static int sumaDodatnich(int tab[]) {
         int suma = 0;
-        for(int i = 0; i < tab.length;i++) {
+        for (int i = 0; i < tab.length; i++) {
             if (tab[i] > 0) {
                 suma += tab[i];
             }
@@ -132,28 +132,26 @@ public class Start {
         return suma;
     }
 
-    public static int sumaUjemnych(int tab[]){
+    public static int sumaUjemnych(int tab[]) {
         int suma = 0;
-        for(int i = 0; i < tab.length;i++) {
-            if (tab[i] < 0){
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] < 0) {
                 suma += tab[i];
             }
         }
         return suma;
     }
 
-    public static int dlugoscMaksymalnegoCiaguDodatnich(int tab[]){
+    public static int dlugoscMaksymalnegoCiaguDodatnich(int tab[]) {
         int licznik = 0;
         int maks = tab.length;
-        for(int i = 0; i < tab.length;i++) {
-            if (tab[i] > 0){
-                licznik +=1;
-            }
-            else if(licznik > maks){
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] > 0) {
+                licznik += 1;
+            } else if (licznik > maks) {
                 maks = licznik;
                 licznik = 0;
-            }
-            else{
+            } else {
                 licznik = 0;
             }
 
@@ -161,38 +159,28 @@ public class Start {
         return licznik;
     }
 
-    public static void signum(int tab[]){
-        for(int i = 0; i < tab.length;i++) {
-            if(tab[i] < 0)
+    public static void signum(int tab[]) {
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] < 0)
                 tab[i] = -1;
-            else if(tab[i] > 0)
+            else if (tab[i] > 0)
                 tab[i] = 1;
-            else if(tab[i] == 0)
+            else if (tab[i] == 0)
                 tab[i] = 0;
         }
     }
 
-    public static int[] odwrocFragment(int[] tab, int lewy, int prawy){
-        if(prawy < lewy) {
-            int bufor = lewy;
-            lewy = prawy;
-            prawy = bufor;
-        }
-        int bufor;
-        for(int i = 0; i <=tab.length-1; i++){
-            for(int j = tab.length-1; j>=0; j-- ){
-                if(lewy-1 >= prawy-1)
-                    break;
-                if(i == lewy-1 && j == prawy-1){
-                    bufor = tab[lewy-1];
-                    tab[lewy-1] = tab[prawy-1];
-                    tab[prawy-1] = bufor;
-                    lewy++;
-                    prawy--;
-                }
+    public static void odwrocFragment(int[] tab, int lewy, int prawy) {
+        if ((lewy > 0) || (lewy < tab.length - 1) || (prawy > 0) || (prawy < tab.length - 1)) {
+            int tab2[] = new int[prawy - lewy + 1];
+            if (prawy + 1 - lewy >= 0)
+                System.arraycopy(tab, lewy, tab2, 0, prawy - lewy + 1);
+            for (int x = lewy; x < prawy + 1; x++) {
+                tab[x] = tab2[prawy - x];
             }
+        } else {
+            System.out.println("Błędne parametry!\n");
         }
-        return tab;
     }
 }
 
